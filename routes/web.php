2abb9c3e9cf17f495da4658753edac225974b649
Abdel-Rahman Fathy <105522928+Abdel-Rahman-Fathy@users.noreply.github.com>
2024-01-16 04:23:47 +0200

    add chinatown

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::get('/clear-cache', function () {
  Route::get('highline', [HomeController::class,'highline'] )->name('highline');
  Route::get('/edge', function () {return view('website.edge.edge');})->name('edge');
 Route::get('united_nations_headquarters', function () {return view('website.united_nations_headquarters.united_nations_headquarters');})->name('united_nations_headquarters');
+Route::get('Chinatown', function () {return view('website.Chinatown.Chinatown');})->name('Chinatown');
 
 
 Route::post('message', [HomeController::class,'message'])->name('message');
